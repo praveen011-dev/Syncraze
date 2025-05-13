5f4b76cd4ed70b0fac5a9dc8180e0f640f93088f
praveen <palpraveen3221@gmail.com>
2025-05-13 19:04:30 +0530

    fix :resolve some issues

--- src/app.js
+++ src/app.js
@@ -26,10 +26,11 @@ app.use("/api/v1/projects/:projectId/tasks", taskroutes);
 //global error handler
 app.use((err, _req, res, _next) => {
   const statusCode = err.statusCode || 500;
-  res.status(statusCode).json({
+  let message=err.message || "Internal Server Error";
+    res.status(statusCode).json({
     success: false,
     statusCode,
-    message: err.message || "Internal Server Error",
+    message,
     errors: err.errors || [],
     stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
   });
--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-
 const connectDB= async()=>{
     try {
        await mongoose.connect(process.env.MONGO_URL);
